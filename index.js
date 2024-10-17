8e9e8609ae835288ecf9170b9d61d113223b795e
William <wily.calvera@gmail.com>
2024-10-16 22:03:45 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    let $hi = document.querySelector("h1")
+    let $p= document.querySelector("#miid")
+    console.log($hi);
+    console.log($p);
+    console.log($hi.textContent)
+}
